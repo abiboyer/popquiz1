5fb1d34d5808f8be949a0d183d71b9ccb58091ab
abiboyer <70119006+abiboyer@users.noreply.github.com>
2020-09-21 10:59:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var number = 5
+var number1 = number(prompt("Input a number."))
+if(isNaN==prompter){
+  document.body.querySelector.innerHTML="Input a number.";
+}
+ var number = number +number1
+if(number <= 10){
+   wrapperEle.innerHTML="Program is over.";
+}
+if(number > 10){
+   wrapperEle.innerHTML="Input another number.";
+}
